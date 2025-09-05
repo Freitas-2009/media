07c2e02fefa4858668a7905a0d7a7a70df46f57d
Freitas-2009 <luisfernando.cfreitas@gmail.com>
2025-09-05 13:17:47 -0300

    Update Média.py

--- Média.py
+++ Média.py
@@ -1,8 +1,21 @@
+
+print("-------------------------------------------------------------------------------")
 nome = input("Digite seu nome: ")
-nota1 = float(input("digite sua primeira nota"))
-nota2 = float(input("digite sua segunda nota"))
-nota3 = float(input("digite sua terceira nota"))
+nota1 = float(input("digite sua primeira nota: "))
+nota2 = float(input("digite sua segunda nota: "))
+nota3 = float(input("digite sua terceira nota: "))
 media = (nota1 + nota2 + nota3) / 3 
+print("-------------------------------------------------------------------------------")
+print (f"O nome do aluno é: {nome}")
+print (f"A média do aluno é: {media}")
 
+if media > 7:
+    print("O aluno está APROVADO ✅")
 
-print (f"O nome do aluno é: {nome} e a média é: {media}")
+elif media > 4:
+    print("O aluno está em RECUPERAÇÃO ⚠️")
+
+else:
+    print("O aluno está REPROVADO ❌")
+
+print("-------------------------------------------------------------------------------")
